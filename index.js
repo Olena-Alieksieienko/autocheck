ea29d35f55ec678961cc7f1f37dcd3dd571f9c89
Olena-Alieksieienko <wleno4kaw@gmail.com>
2022-12-28 18:26:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// # Модуль 1. Заняття 1. Змінні, типи та оператори
+
+// ## Example 1 - Математичні оператори
+
+// Виведи на екран загальну кількість всіх яблук.
+// кількість хороших яблук.
+
+// ```js
+const apples = 470;
+const badApples = 135;
+const halfApples = 67;
+
+const harmfulApples = badApples + halfApples;
+console.log(harmfulApples);
+
+const goodApples = apples - harmfulApples;
+console.log(goodApples);
+// ```
